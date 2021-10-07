87a55f80787af6981e42da372bc16f2f3b4d25fb
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-10-07 21:18:48 +0100

    up to part 6

--- Chapter4/drill1_whileloop.cpp
+++ Chapter4/drill1_whileloop.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
-#include <string>
+#include <limits>
+#include <iomanip>
 
 int main()
 {
-    std::string a;
-    std::string b;
+    double a;
+    double b;
+    bool cond {false};
 
-    while(std::cin >> a >> b){
-        if (a == "|" || b == "|")
-            break;
-        else
-            std::cout << "a: " << a << "\nb: " << b << "\n";
+    std::setprecision(51);
+    while(!cond){
+        if (std::cin >> a >> b){
+            if (a == b)
+                std::cout << "The numbers are equal\n";
+            else{
+                std::cout << "The smaller value is: " << ((a < b )? a : b);
+                std::cout << "\nThe larger value is: " << ((a > b)? a : b);
+                std::cout << '\n';
+                if ( abs(a - b) < 0.01){
+                    std::cout << "The numbers are almost equal\n";
+                }
+            }
+        }
+        else {
+            std::cin.clear();    // reset the fail state
+            if (std::cin.peek() == '|') {
+                std::cout << "Found end of input marker |\n";
+                cond = true;
+            } else {
+                std::cout << "Invalid input!\n";
+            }
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
 }
